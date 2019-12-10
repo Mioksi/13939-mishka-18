aff9a398cd8e609a4d4cfd5dfc3ea86e4619fd51
Mioksi <mio.natali@hotmail.com>
2019-12-10 17:06:29 +0300

    Переделывает блок page-header

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
-var navigationMain = document.querySelector('.main-navigation');
-var navigationToggle = document.querySelector('.main-navigation__toggle');
+var pageHeader = document.querySelector('.page-header__wrapper');
+var navigationToggle = document.querySelector('.page-header__toggle');
 
-navigationMain.classList.remove('main-navigation--nojs');
+pageHeader.classList.remove('page-header__wrapper--menu-nojs');
 
 navigationToggle.addEventListener('click', function() {
-  if (navigationMain.classList.contains('main-navigation--closed')) {
-    navigationMain.classList.remove('main-navigation--closed');
-    navigationMain.classList.add('main-navigation--opened');
+  if (pageHeader.classList.contains('page-header__wrapper--menu-closed')) {
+    pageHeader.classList.remove('page-header__wrapper--menu-closed');
+    pageHeader.classList.add('page-header__wrapper--menu-opened');
   } else {
-    navigationMain.classList.add('main-navigation--closed');
-    navigationMain.classList.remove('main-navigation--opened');
+    pageHeader.classList.add('page-header__wrapper--menu-closed');
+    pageHeader.classList.remove('page-header__wrapper--menu-opened');
   }
 });
 
